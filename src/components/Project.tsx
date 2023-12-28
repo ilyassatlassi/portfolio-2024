b6d47c351b21064d7c411b773c7052ebc84b93f6
ilyass atlassi <atlassiilyass@gmail.com>
2023-12-28 23:49:38 +0100

    add drawzer

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -85,6 +85,7 @@ export default function Project({
         group-hover:translate-y-3
         group-hover:-rotate-2
 
+        
         group-even:group-hover:translate-x-3
         group-even:group-hover:translate-y-3
         group-even:group-hover:rotate-2
